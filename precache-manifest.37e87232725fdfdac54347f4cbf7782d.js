ec24785076f996d764bb0eacd39d74b6a045e333
Yash Naik <yashnaik47@gmail.com>
2019-07-01 13:36:18 +0530

    Updates

---
+++ precache-manifest.37e87232725fdfdac54347f4cbf7782d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce464c7b76cd038ec0ae287c3d3b54d0",
+    "url": "/test7/index.html"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/test7/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "bbab89be8bfd8efe5df4",
+    "url": "/test7/static/js/main.fca92717.chunk.js"
+  },
+  {
+    "revision": "05bba542e0a8098878a5",
+    "url": "/test7/static/js/runtime~main.ec69e020.js"
+  }
+]);
